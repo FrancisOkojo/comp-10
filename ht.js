bdadb782355b1682d4bc95819a9127f5207973ea
Blackboxx-x <asheralexander98@gmail.com>
2025-10-03 13:18:33 -0600

    Add files via upload

---
+++ ht.js
@@ -0,0 +1,16 @@
+VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x4a0559,
+    shininess: 0.00,
+    waveHeight: 35.50,
+    waveSpeed: 0.60,
+    zoom: 1.50
+  });
+  
