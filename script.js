0ffb11f5d00ea37d03d5af85d8b5066901ad4e78
FrancisOkojo <aaronokojo6@gmail.com>
2025-10-31 13:07:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+if (document.getElementById('vanta-bg')) {
+  VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x4a0559,       
+    shininess: 80.00,
+    waveHeight: 15.00,
+    waveSpeed: 1.20,
+    zoom: 0.85
+  });
+}
